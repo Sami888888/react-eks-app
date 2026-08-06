821ff39d8c71a0ea018416758f5ff616404cea83
Sami888888 <sami776e@gmail.com>
2026-08-07 03:53:44 +0530

    Testing auto deploy via webhook

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
       <h2>React Application Deployed on Amazon EKS</h2>
 
       <p>Developed by MD Sami Ansari</p>
-
       <h3>Jenkins • Docker • Kubernetes • AWS EKS</h3>
+      <h1>updated the heading to something like 'CI/CD Pipeline Auto-Deploy Test 🎉</h1>
     </div>
   );
 }
