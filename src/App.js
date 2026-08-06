6b96432895e3bab4876b160551289fce1671bf77
MD Sami Ansari <sami776e@gmail.com>
2026-08-07 02:36:46 +0530

    Added Jenkins pipeline

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "80px",
+        fontFamily: "Arial, sans-serif"
+      }}
+    >
+      <h1>AWS DevOps CI/CD Project</h1>
+
+      <h2>React Application Deployed on Amazon EKS</h2>
+
+      <p>Developed by MD Sami Ansari</p>
+
+      <h3>Jenkins • Docker • Kubernetes • AWS EKS</h3>
+    </div>
+  );
+}
+
+export default App;
